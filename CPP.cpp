b7feb0b86f6c767a708e68d915dd11abbcd2e50a
Patryk Lukaszewski <lukaszewski.patryk06@gmail.com>
2021-10-15 14:50:56 +0200

    Test

--- CPP.cpp
+++ CPP.cpp
@@ -5,7 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!!\n"<<"H";
+
+
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
